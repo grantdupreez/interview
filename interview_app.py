64245787502174f9f4a16cd4bb3264103dba82f4
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2025-03-09 13:51:09 +0000

    Update interview_app.py

--- interview_app.py
+++ interview_app.py
@@ -13,12 +13,54 @@ from langchain_openai import ChatOpenAI
 from openai import OpenAI
 from langchain.retrievers.multi_query import MultiQueryRetriever
 import os
+import hmac
 import io
 from audio_recorder_streamlit import audio_recorder
 
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+if not check_password():
+    st.stop()
+
+
+
+
 conversation_placeholder = st.empty()
 
-API_KEY = st.secrets.get('OPENAI_API_KEY')
+API_KEY = st.secrets.["auth_key"])
 
 def clear_chat():
     chat_history.clear()
