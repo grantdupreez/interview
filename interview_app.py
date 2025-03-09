f874cce289c0b147c4c9dee7c039a8f1133964ea
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2025-03-09 14:15:08 +0000

    Update interview_app.py

--- interview_app.py
+++ interview_app.py
@@ -156,7 +156,7 @@ if 'loaded_documents' not in st.session_state:
     loaded_documents = load_documents_from_directory(data_directory)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
     texts = text_splitter.split_documents(loaded_documents)
-    embeddings = OpenAIEmbeddings(openai_api_key)
+#    embeddings = OpenAIEmbeddings(openai_api_key)
     embeddings = OpenAIEmbeddings(model='text-embedding-3-large')
 
     persist_directory = 'chroma'
